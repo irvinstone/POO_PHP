dadb31815345cb0d30861bd9a97a1b38e5b603ee
irvin <irvinlucho@gmail.com>
2018-09-27 11:06:43 -0500

    feat(poo): Agregar comentarios,explicacion y conceptos usados

--- Poo.php
+++ Poo.php
@@ -6,7 +6,11 @@
  * Time: 2:50 PM
  */
 
-
+/**
+ * Clase abstracta: clase que no podrá ser instanciada,
+ * Usada para implementar características comunes que tendrán las clases que heredan esta clase.
+ * Class Animal
+ */
 abstract class Animal
 {
     public function comer()
@@ -19,11 +23,22 @@ abstract class Animal
         echo "duermiendo";
     }
 
+    /**
+     * Método abstracto: usado para definir metodos que obligan a ser implementados por las clases herederas.
+     */
     public abstract function hacerRuido();
 }
 
+/**
+ * Herencia
+ * Clase que hereda una clase abstracta e implemnta una interfaz.
+ * Class Perro
+ */
 class Perro extends Animal implements AnimalInterface
 {
+    /**
+     * Método heredado  obligado a implementar.
+     */
     public function hacerRuido()
     {
         echo "Wuof Wuof!!\n";
@@ -38,9 +53,24 @@ class Gato extends Animal implements AnimalInterface
     }
 }
 
+/**
+ * Class Humano: implementa 2 interfaces puesto que cumple com ambas
+ */
 class Humano extends Animal implements HumanoInterface, AnimalInterface
 {
     protected $mascota;
+    protected $padre;
+
+    /**
+     * Humano constructor:
+     * Injección de dependencias,Polimorfismo por sobrecarga(distinto en otros lenguajes como java o c++
+     * @param $padre
+     */
+    public function __construct(Humano $padre = null)
+    {
+        $this->padre = $padre;
+    }
+
     public function comer()
     {
         echo "comiendo sofisticadamente";
@@ -56,12 +86,23 @@ class Humano extends Animal implements HumanoInterface, AnimalInterface
         echo "aaaaaaaaaaaaaaaaaaa";
     }
 
+    /**Método abstracto:
+     * Aplica inversión de dependencias.
+     * puesto que el humano puede adoptar cualquier tipo de mascota.
+     * no se usa una clase en particular como parametro sino una interfaz que represente a todos estos.
+     * @param AnimalInterface $animal
+     */
     public function adoptarMascota(AnimalInterface $animal)
     {
         $this->mascota = $animal;
         $animal->hacerRuido();
     }
 }
+
+/**
+ * Usado para que las clases , obligadamente implementen los siguientes métodos.
+ * Interface AnimalInterface
+ */
 Interface AnimalInterface
 {
     function hacerRuido();
@@ -82,19 +123,30 @@ Interface HumanoInterface
     function adoptarMascota(AnimalInterface $animal);
 }
 
+/**
+ * Clase principal donde nuestros objeos comparten mensajes.
+ * Class Life
+ */
 class Life
 {
+    /**
+     * Metodo abstracto: el cual no requiere que la clase estee instanciada.
+     */
     public static function iniciarCicloVida()
     {
-        $human = new Humano();
-        $gato  = new Gato();
-        $perro = new Perro();
-        $human->adoptarMascota($gato);
-        $human->adoptarMascota($perro);
-        $human->comer();
+        $padre    = new Humano();
+        $personaX = new Humano($padre);
+        $gato     = new Gato();
+        $perro    = new Perro();
+        $personaX->adoptarMascota($gato);
+        $personaX->adoptarMascota($perro);
+        $personaX->comer();
     }
 }
 
+/**
+ * Ejecución del programa
+ */
 Life::iniciarCicloVida();
 
 
